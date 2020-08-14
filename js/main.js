bba1955eceef131b2010a08cdafdb85088a878e2
andrey373 <60426907+andrey373@users.noreply.github.com>
2020-08-14 21:14:29 +0300

    develop cards product

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 $(function(){
-
-    $('.tabs-btn__link').on('click', function(evt){
+    // script for button tabs 
+    $('.press-tab').on('click', function(evt){
         evt.preventDefault();
 
-        $('.tabs-btn__link').removeClass('tabs-btn__link--active');
-        $('.search-block__content-item').removeClass('search-block__content-item--active');
-        $(this).addClass('tabs-btn__link--active');
+        $('.press-tab').removeClass('press-tab--active');
+        $('.search-block__content-item').removeClass('press-tab-content--active');
+        $(this).addClass('press-tab--active');
         
-        $($(this).attr('href')).addClass('search-block__content-item--active');
-    })   
+        $($(this).attr('href')).addClass('press-tab-content--active');
+    })
+    
+    //  script for button heart in card product
+
+    $('.card-product__slider-favorites').on('click', function(){
+        $(this).toggleClass('card-product__slider-favorites--active');
+    });
 });
 
 
