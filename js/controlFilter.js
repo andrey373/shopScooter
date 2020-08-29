9c33f6b3e59cacfb4cc5ef7342b2e146e354d9c9
andrey373 <60426907+andrey373@users.noreply.github.com>
2020-08-29 19:11:38 +0300

    completion product page

--- js/controlFilter.js
+++ js/controlFilter.js
@@ -1,10 +1,12 @@
 $(function(){
     
     // script for settings filter
+    // https://github.com/Dimox/jQueryFormStyler
     $('.filterStyle').styler();
 
 
-    // script for input type range
+    // script for input type range 
+    // https://github.com/IonDen/ion.rangeSlider 
     $(".js-range-slider").ionRangeSlider({
     });
 
@@ -12,6 +14,16 @@ $(function(){
     $('.sale-item-box').on('click', function(){
         $('.sale-item-box').removeClass('checkedSale');
         $(this).toggleClass('checkedSale');
+    });
+
+    // script for settings pluging rate-yo
+    // https://github.com/prrashi/rateYo
+    $("#rateYo").rateYo({
+        ratedFill: "#1C62CD",
+        normalFill: "#c4c4c4",
+        starWidth: "23px",
+        spacing: "7px",
+        readOnly: true
     });
 
 });
