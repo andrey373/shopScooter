18a13a3073c18e6ab59fdadc70285d4c096449b1
andrey373 <60426907+andrey373@users.noreply.github.com>
2020-09-03 18:40:37 +0300

    completion responsive

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ $(function(){
         evt.preventDefault();
 
         $($(this).siblings()).removeClass('press-tab--active');
-        $($(this).parent().siblings().find('div')).removeClass('press-tab-content--active');
+        $($(this).closest('.search-block__tabs-btn-wraper').siblings().find('div')).removeClass('press-tab-content--active');
         $(this).addClass('press-tab--active');
         
         $($(this).attr('href')).addClass('press-tab-content--active');
+
+        $('.card-product__slider').slick('setPosition')
     })
     
     //  script for button heart in card product
@@ -35,6 +37,12 @@ $(function(){
         $('.menu-mobile-list').toggleClass('menu-mobile-list--active');
     });
 
+    // script for list item footer
+
+    $('.page-footer__top-item-closed').click(function(){
+        $(this).toggleClass('page-footer__top-item-open');
+        $(this).children('.footer__list').toggleClass('footer__list--active');
+    });
 });
 
 
